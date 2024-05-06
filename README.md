14ec0cfecd5cf58dd267cc6aab20b82f79f97807
iamcynthia <alicia92429@outlook.com>
2024-05-06 14:04:22 +0800

    remove unrelated files

--- README.md
+++
@@ -1,5 +0,0 @@
-# MSc_Database_CW1
-
-2019/11 --- Database Coursework 01
-
-For more information, please check the file "assignment1.pdf"
--- delete.sql
+++
@@ -1,9 +0,0 @@
--- Part 1.6 delete.sql
---
--- Submitted by: Hsin-Ju Chan
--- 
--- 
-
-
--- add your statements here
-DELETE FROM phdStudent WHERE id IN (SELECT student_id FROM supervises GROUP BY student_id Having SUM(type) = 1); 
