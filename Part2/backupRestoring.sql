fd46caefabe6a58af23fec720430f9aa0cb161c1
iamcynthia <alicia92429@outlook.com>
2024-05-06 14:10:59 +0800

    remove unrelated files

--- Part2/backupRestoring.sql
+++
@@ -1,8 +0,0 @@
--- Part 1.1 backupRestoring.sql
---
--- Submitted by: Hsin -Ju, Chan
--- 
-
-
--- add your Backup Restoring statements here
-source ./db/crimes2015.sql
--- README.md
+++
@@ -1,5 +0,0 @@
-# MSc_Database_CW2
-
-2019/12 --- Database Coursework 02
-
-For more information, please check file "Assignment2.pdf"
